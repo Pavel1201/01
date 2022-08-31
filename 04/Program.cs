c08d2969f249ef10fdb26e1922b7b0a02155ff3a
Pavel <p.a.dudarev@mail.ru>
2022-09-01 04:58:18 +0500

    Task 04

---
+++ 04/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Введите число ");
+
+string input = Console.ReadLine();
+
+int number = int.Parse(input);
+
+Console.WriteLine($"Последняя цифра трехзначного числа это: {number%10}");
